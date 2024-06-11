82e809bf2f3ed8542ca8a4dd600ff2c22cbfbcb9
namkhanh.nguyen <namkhanh.nguyen@DE-BYFZFB3.ad.diebold.com>
2024-06-11 16:38:23 +0200

    refactored Main class

--- src/bvk_ss23/ImageCompressionApp.java
+++ src/bvk_ss23/ImageCompressionApp.java
@@ -18,7 +18,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-public class Main extends Application
+public class ImageCompressionApp extends Application
 {
 
     @Override
